28312bbc7f02ff93912dc9b73090cd570d351c45
Nick Knecht <nick.knecht@hotmail.com>
2019-11-27 15:58:00 +0100

    initial commit

---
+++ Objects/test.d
@@ -0,0 +1,8 @@
+.\objects\test.o: test.c
+.\objects\test.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\stm32f10x.h
+.\objects\test.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm3.h
+.\objects\test.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
+.\objects\test.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
+.\objects\test.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.2.0\Device\Include\system_stm32f10x.h
